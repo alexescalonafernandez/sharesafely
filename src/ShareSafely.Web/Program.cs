cbbacb03508b1f1359d3495331c0969eb0440c23
aefernandez <alexescalonafernandez@gmail.com>
2026-05-04 17:54:50 +0200

    feat: add Application Insights telemetry

--- src/ShareSafely.Web/Program.cs
+++ src/ShareSafely.Web/Program.cs
@@ -11,6 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddApplicationInsightsTelemetry();
 
 builder.Services.Configure<UploadOptions>(
     builder.Configuration.GetSection("Upload")
